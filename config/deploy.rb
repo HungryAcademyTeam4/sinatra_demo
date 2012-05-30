1002c338f18a52cf70608a291e8ab326c4f6109e
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-05-30 19:46:44 -0400

    Add gemfile & lock

---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+require 'bundler/capistrano'
+
+server "50.116.60.38", :web, :db, :app, primary: true
+set :user, "root"
+set :application, "Demobot"
+
+set :deploy_to, "/home/apps/#{application}"
+set :deploy_via, :remote_cache
+
+
+set :scm, "git"
+set :repository,  "git@github.com:HungryAcademyTeam4/sinatra_demo.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+
